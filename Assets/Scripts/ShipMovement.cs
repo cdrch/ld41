b9c6bd04af7cfe98c992712e7632154cc2f7d990
cdrch <carterdavidhenning@gmail.com>
2018-04-22 14:25:23 -0400

    Minor changes

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -10,6 +10,9 @@ public class ShipMovement : MonoBehaviour
     public int verticalInvert = 1;
     public int horizontalInvert = 1;
 
+    public float xMoveLimit = 5f;
+    public float yMoveLimit = 5f;
+
     // Use this for initialization
     void Start ()
     {
@@ -22,12 +25,46 @@ public class ShipMovement : MonoBehaviour
         float horizontal =  Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+
+
         Vector3 direction = new Vector3(horizontalInvert * horizontal, verticalInvert * vertical, 0f);
         Vector3 finalDirection = new Vector3(horizontalInvert * horizontal, verticalInvert * vertical, 1f);
-
+        
+        if ((transform.localPosition + (direction * movementSpeed * Time.deltaTime)).x > xMoveLimit || (transform.localPosition + (direction * movementSpeed * Time.deltaTime)).x < -xMoveLimit)
+        {
+            direction = new Vector3(0f, direction.y, direction.z);
+            finalDirection = new Vector3(0f, finalDirection.y, finalDirection.z);
+        }
+        
+        if ((transform.localPosition + (direction * movementSpeed * Time.deltaTime)).y > yMoveLimit || (transform.localPosition + (direction * movementSpeed * Time.deltaTime)).y < -yMoveLimit)
+        {
+            direction = new Vector3(direction.x, 0f, direction.z);
+            finalDirection = new Vector3(finalDirection.x, 0f, finalDirection.z);
+        }
+        
         transform.position += direction * movementSpeed * Time.deltaTime;
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(finalDirection), Mathf.Deg2Rad * 50.0f);
         
 	}
+
+    public void Ascend()
+    {
+        //bool pastTopLimit = 
+    }
+
+    public void Descend()
+    {
+
+    }
+
+    public void BankLeft()
+    {
+
+    }
+
+    public void BankRight()
+    {
+
+    }
 }
